49629b0f29ae6b7386ec06421adbd2f91c12c1ba
sajibur-rahman <sajiburr21@gmail.com>
2022-03-26 02:58:55 +0600

    nav bar is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import NavBar from './Components/NavBar/NavBar';
 import Shop from './Components/Shop/Shop';
 
 const App = () => {
   return (
     <div>
+      <NavBar/>
       <Shop/>
     </div>
   );
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+const NavBar = () => {
+    return (
+        <div className='nav'>
+            <h1 className='company-name'>bongo shop</h1>
+            <p>choose  4 product</p>
+        </div>
+    );
+};
+
+export default NavBar;
